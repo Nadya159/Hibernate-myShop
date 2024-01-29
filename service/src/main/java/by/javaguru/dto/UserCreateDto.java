a5c116111ab5fecb962881a40aa00a0029561193
Nadya159 <138751846+Nadya159@users.noreply.github.com>
2024-01-29 22:55:27 +0500

    Update UserCreateDto.java

--- service/src/main/java/by/javaguru/dto/UserCreateDto.java
+++ service/src/main/java/by/javaguru/dto/UserCreateDto.java
@@ -2,15 +2,22 @@ package by.javaguru.dto;
 
 import by.javaguru.entity.Gender;
 import by.javaguru.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record UserCreateDto(
+        @NotEmpty(message = "{name_invalid}")
         String name,
+        @NotEmpty(message = "{phone_invalid}")
+        @Size(min=11, max=11, message = "{phone_invalid}")
         String phone,
-        String email,
+        @Email String email,
         Role role,
         Gender gender,
         LocalDate birthday,
+        @NotEmpty(message = "{pswd_invalid}")
         String password) {
 }
